c5888f20cfaff8c6ab644aadc434a2d469d189bd
Ashok Kumar <ashokkumar@MacBook-Pro.local>
2025-07-02 21:55:26 +0530

    clean ups

--- run_test_script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Navigate to the project directory
-cd /Users/ashokkumar/Desktop/test/
-
-# Activate the virtual environment
-source .venv/bin/activate
-
-# Run the Python script
-/Users/ashokkumar/Desktop/test/.venv/bin/python /Users/ashokkumar/Desktop/test/stock_work/test_cron_script.py
--- test1.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /Users/ashokkumar/Desktop/test/
-/Users/ashokkumar/Desktop/test/.venv/bin/activate
-/Users/ashokkumar/Desktop/test/.venv/bin/python3 /Users/ashokkumar/Desktop/test/stock_work/test_cron1.py >> /Users/ashokkumar/Desktop/test/stock_work/out.txt
